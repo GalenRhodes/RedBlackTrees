07fc7b31d6c97e82dfa16a479f2a02e059722511
Galen Rhodes <galenrhodes@me.com>
2018-03-25 11:30:11 -0400

    Added Test App.

---
+++ RedBlackTreesFramework/RedBlackTreesFramework.h
@@ -0,0 +1,17 @@
+//
+//  RedBlackTreesFramework.h
+//  RedBlackTreesFramework
+//
+//  Created by Galen Rhodes on 3/25/18.
+//  Copyright © 2018 Project Galen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+FOUNDATION_EXPORT double              RedBlackTreesFrameworkVersionNumber;
+FOUNDATION_EXPORT const unsigned char RedBlackTreesFrameworkVersionString[];
+
+#import <RedBlackTreesFramework/PGRBTNode.h>
+#import <RedBlackTreesFramework/PGRBTMap.h>
+#import <RedBlackTreesFramework/PGRBTDrawingTools.h>
+
